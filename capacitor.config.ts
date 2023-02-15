84db911de8de9cc874402d751617d505e6f629f4
OmarMAARAF <o.marmaaraf0@gmail.com>
2023-02-15 16:46:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'gym.fit',
+  appName: 'gym-fit',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
