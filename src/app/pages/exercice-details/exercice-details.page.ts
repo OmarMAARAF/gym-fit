e93691f47afe5f6ef0a0f118c87d3b48ac4bf7e1
OmarMAARAF <o.marmaaraf0@gmail.com>
2023-02-16 23:22:13 +0100

    adding exercice list

--- src/app/pages/exercice-details/exercice-details.page.ts
+++ src/app/pages/exercice-details/exercice-details.page.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
-
+import {Exercice} from "../../services/Exercice"
 @Component({
   selector: 'app-exercice-details',
   templateUrl: './exercice-details.page.html',
   styleUrls: ['./exercice-details.page.scss'],
 })
 export class ExerciceDetailsPage implements OnInit {
-
+  @ViewChild('canvas') canvas: ElementRef;
+  exercice :any
+  gifUrl:string
   constructor(private router: Router,private actRoute: ActivatedRoute) { 
     console.log(this.router.getCurrentNavigation()!.extras.state)
+    this.exercice =this.router.getCurrentNavigation()!.extras.state!['exercice']
+    this.gifUrl=this.exercice['gifUrl']
+  }
+  onGifLoad(event: Event) {
+    const img = event.target as HTMLImageElement;
+    console.log(img)
+    console.log(this.canvas)
+    const canvas = this.canvas.nativeElement;
+    canvas.width = img.width;
+    canvas.height = img.height;
+    canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
   }
 
   ngOnInit() {
-    /* this.actRoute.data.subscribe(data => {
-      console.log(data);
-  }) */
+    
   }
 
 }
--- src/app/pages/exercices-list/exercices-list.page.ts
+++ src/app/pages/exercices-list/exercices-list.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
-
+import axios from 'axios';
+import {Exercice} from "../../services/Exercice"
 @Component({
   selector: 'app-exercices-list',
   templateUrl: './exercices-list.page.html',
@@ -8,13 +9,32 @@ import { ActivatedRoute, Router } from "@angular/router";
 })
 export class ExercicesListPage implements OnInit {
 
+  ExrcicesList : Exercice []
   bodyPart:string | null ;
   constructor(private router: Router,private actRoute: ActivatedRoute) { 
     this.bodyPart=this.actRoute.snapshot.paramMap.get('bodyPart');
     console.log(this.bodyPart)
+    
   }
 
   ngOnInit() {
+    //get list of exercices:
+    const options = {
+      method: 'GET',
+      url: `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${this.bodyPart}`,
+      headers: {
+        'X-RapidAPI-Key': '23d1c2af11msha06cfac7c98760dp100742jsn512292d57b35',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      }
+    };
+    axios.request(options).then(response=> {
+      this.ExrcicesList=response.data.slice(0,16)
+      console.log("list",this.ExrcicesList)
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
   }
 
+  
 }
