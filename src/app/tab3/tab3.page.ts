6afd7d3ed3da4ca36abcf594353bc3a35d3182ae
OmarMAARAF <o.marmaaraf0@gmail.com>
2023-02-19 20:49:22 +0100

    get bookmarks list from db

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { BookmarkService } from './../services/bookmark.service';
 import { ToastController } from '@ionic/angular';
+import {Exercice} from "../services/Exercice"
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
+  ExeciseList:Exercice[]=[];
   constructor(
     private BookmarkService: BookmarkService,
     private toastController: ToastController
   ) {}
 
+  ngOnInit() {
+    this.BookmarkService.getExerciseList()
+      .snapshotChanges()
+      .subscribe((res): any => {
+        res.forEach((item) => {
+          let a: any = item.payload.toJSON();
+          console.log(a)
+          this.ExeciseList.push(a)
+        });
+        console.log(this.ExeciseList)
+      });
+  }
 }
