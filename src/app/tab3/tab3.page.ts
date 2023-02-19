c3d3f9f2e075561ea92cb2fdaef7ba3ea2155277
OmarMAARAF <o.marmaaraf0@gmail.com>
2023-02-19 22:56:41 +0100

    add bookmark list design

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit,ViewChild, ElementRef  } from '@angular/core';
 import { BookmarkService } from './../services/bookmark.service';
 import { ToastController } from '@ionic/angular';
 import {Exercice} from "../services/Exercice"
@@ -9,14 +9,26 @@ import {Exercice} from "../services/Exercice"
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page implements OnInit {
-
+  @ViewChild('canvas') canvas: ElementRef;
+  onGifLoad(event: Event, exercice: Exercice) {
+    const img = event.target as HTMLImageElement;
+    const skeleton= img!.previousElementSibling
+    const canvas = img!.nextElementSibling as HTMLCanvasElement;
+    canvas.width = img.width;
+    canvas.height = img.height;
+    canvas!.getContext('2d')!.drawImage(img, 0, 0, img.width, img.height);
+    img.remove();
+    skeleton!.remove()
+  }
   ExeciseList:Exercice[]=[];
   constructor(
     private BookmarkService: BookmarkService,
     private toastController: ToastController
   ) {}
+  
 
   ngOnInit() {
+    
     this.BookmarkService.getExerciseList()
       .snapshotChanges()
       .subscribe((res): any => {
