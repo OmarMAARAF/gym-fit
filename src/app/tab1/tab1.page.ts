3bf91fc65ce34c597b9f52d9b27e8908ce918731
OmarMAARAF <o.marmaaraf0@gmail.com>
2023-02-22 22:50:08 +0100

    add search bar

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  search:string
 
+  handleChange(e:Event){
+    console.log(e)
+   // console.log(e.target!.value.toLowerCase())
+   console.log(this.search)
+  }
   constructor() {}
 
 }
