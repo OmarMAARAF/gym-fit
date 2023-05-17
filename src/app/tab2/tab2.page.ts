a1c44fd55b65483b6af7911cd0f697728d5898d0
OmarMAARAF <o.marmaaraf0@gmail.com>
2023-05-17 19:51:08 +0100

    create the calendar view

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,140 +1,74 @@
-import { formatDate } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { Component ,ViewChild, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-
-  
-  @ViewChild('myDatetime', { static: false }) myDatetime !: Date;
-  selectedDate !: string;
-  maxDate !: string;
-  yearValue !: number;
-  currentDate: string = formatDate(new Date(), 'yyyy-MM-dd', 'en-US');
+export class Tab2Page  {
+  currentDate: Date;
+  weekDates: Date[];
+  weekDays: Date[];
+  clickedDate: Date | null;
 
   constructor() {
-    /*To make the calendar show one month only*/
-    const currentDate = new Date();
-    const maxDate = new Date();
-    maxDate.setMonth(currentDate.getMonth() + 1);
-    this.maxDate = maxDate.toISOString();
-
+    this.currentDate = new Date();
+    this.weekDates = this.getWeekDates(this.currentDate);
+    this.weekDays = this.getWeekDays(this.currentDate);
   }
 
-  workOuts  = [
-    {'id': 1, 'name': 'Chest', 'date': '2023-05-01', 'time': '10:00', 'duration': '1:00', 'image': 'assets/workout1.png'},
-    {'id': 2, 'name': 'Back', 'date': '2023-05-02', 'time': '10:00', 'duration': '1:00', 'image': 'assets/workout2.png'},
-    {'id': 3, 'name': 'Legs', 'date': '2023-05-03', 'time': '10:00', 'duration': '1:00', 'image': 'assets/workout3.png'},
-    {'id': 4, 'name': 'Shoulders', 'date': '2023-05-04', 'time': '10:00', 'duration': '1:00', 'image': 'assets/workout4.png'},
-    {'id': 5, 'name': 'Arms', 'date': '2023-05-05', 'time': '10:00', 'duration': '1:00', 'image': 'assets/workout5.png'},
-  ]
-
-  workOutPerDay = this.workOuts;
-
-  ngOnInit() {
+  previousWeek() {
+    this.currentDate.setDate(this.currentDate.getDate() - 7);
+    this.updateWeek();
   }
 
-  ionViewDidEnter() {
-    this.disableMonthSelector();
-  }
-  
-  onDateChange() {
-    let date = this.selectedDate.split('T')[0];
-    console.log(date);
-    for (let i = 0; i < this.workOuts.length; i++) {
-      if (this.workOuts[i].date == date) {
-        //add it to the array
-        this.workOutPerDay = [];
-        this.workOutPerDay.push(this.workOuts[i]);
-        console.log(this.workOutPerDay);
-      }
-    }
-    
+  nextWeek() {
+    this.currentDate.setDate(this.currentDate.getDate() + 7);
+    this.updateWeek();
   }
 
+  updateWeek() {
+    this.weekDates = this.getWeekDates(this.currentDate);
+    this.weekDays = this.getWeekDays(this.currentDate);
+    this.clickedDate = null; // Reset clicked date
+  }
 
-  disableMonthSelector() {
-    const ionDateTimeElement = document.getElementsByTagName('ion-datetime');
-    const element = ionDateTimeElement[0].shadowRoot?.getRootNode().firstChild as HTMLElement;
-
-    const monthsSelector = element.getElementsByClassName(
-    'calendar-month-year'
-    )[0] as HTMLElement;
-    console.log(monthsSelector);
-    monthsSelector.style.pointerEvents = 'none';
-    }
-
-    onWorkOutDetails(id : number) {
-      console.log("Details of workout : " + id);
-    }
-
-    onWorkoutSkip(id : number) {
-      console.log("Skipped workout : " + id);
-    }
-
-
-}
-/* import { Component ,ViewChild, AfterViewInit} from '@angular/core';
-
-@Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
-})
-export class Tab2Page  implements AfterViewInit{
-  @ViewChild('myDiv', { static: false }) myDiv: Element;
-  dates: Date[] = [];
-  selectedDate: Date;
-
-  constructor() {
-    // Initialize the array of dates with the current week's dates
-    const today = new Date();
-    const dayOfWeek = today.getDay();
-    const startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - dayOfWeek);
+  getWeekDates(date: Date): Date[] {
+    const startOfWeek = new Date(date);
+    startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay());
+    const weekDates = [];
     for (let i = 0; i < 7; i++) {
-      const date = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + i);
-      this.dates.push(date);
+      const currentDate = new Date(startOfWeek);
+      currentDate.setDate(currentDate.getDate() + i);
+      weekDates.push(currentDate);
     }
-
-    // Select the current day by default
-    this.selectedDate = new Date();
-    // Add a design to the selected day
-    const todayElement = document.getElementById('day-' + today.getDay());
-    console.log(todayElement)
-    if (todayElement) {
-      todayElement.classList.add('today');
-    }
+    return weekDates;
   }
 
-  handleClick(date: Date): void {
-    // Remove the design from the previously selected day
-    const previousSelectedElement = document.getElementById('day-' + this.selectedDate.getDay());
-    if (previousSelectedElement) {
-      previousSelectedElement.classList.remove('today');
-    }
-
-    // Set the new selected date and add a design to it
-    this.selectedDate = date;
-    const selectedElement = document.getElementById('day-' + this.selectedDate.getDay());
-    if (selectedElement) {
-      selectedElement.classList.add('today');
-    }
-
-    // Get the selected day (e.g. "Monday", "Tuesday", etc.)
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const selectedDay = daysOfWeek[date.getDay()];
-
-    console.log(`Selected day: ${selectedDay}`);
-  }
-  ngAfterViewInit():  void {
-    const today = new Date();
-    this.handleClick(today)
+  getWeekDays(date: Date): Date[] {
+    const weekDates = this.getWeekDates(date);
+    const weekDays = weekDates.map(date => new Date(date));
+    return weekDays;
   }
 
-  
+  getCurrentDayName(): string {
+    return this.weekDays[this.currentDate.getDay()].toLocaleString('default', { weekday: 'long' });
+  }
+
+  isCurrentDay(date: Date): boolean {
+    return date.toDateString() === this.currentDate.toDateString();
+  }
+
+  isClickedDay(date: Date): boolean | null {
+    return this.clickedDate && date.toDateString() === this.clickedDate.toDateString();
+  }
+
+  onDateClick(date: Date): void {
+      this.clickedDate = date
+      this.currentDate = date;
+      const dayName = this.weekDays[date.getDay()].toLocaleString('default', { weekday: 'long' });
+  console.log(dayName);
+  }
 }
- */
+
 
